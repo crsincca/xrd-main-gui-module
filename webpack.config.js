f192bb201a9a41de1bce1f4d8983b5f32759b6a5
yuyuryev <yu.yuryev.dev@gmail.com>
2020-05-14 22:48:11 +0200

    output umd modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
-    entry: './index.jsx',
-    mode: 'development',
+var common = {
+    devtool: 'source-map',
+    externals: {
+        'react': 'react',
+        'react-dom': 'react-dom'
+    },
     module: {
         rules: [
             {
@@ -30,16 +34,38 @@ module.exports = {
                         options: {
                             name: 'images/[name].[ext]'
                         }
-                    }]
+                    }
+                ]
             }
         ]
     },
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        publicPath: '/',
+        filename: '[name].js',
+        library: 'crsincca-xrd-main-gui',
+        libraryTarget: 'umd'
+    },
     resolve: {
         extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: 'module.bundle.js'
     }
 };
+
+var devConfig = merge(common, {
+    entry: {
+        'bundle': './index.jsx'
+    },
+    mode: 'development'
+});
+
+var prodConfig = merge(common, {
+    entry: {
+        'bundle.min': './index.jsx'
+    },
+    optimization: {
+        minimize: true
+    },
+    mode: 'production'
+});
+
+module.exports = [devConfig, prodConfig];
